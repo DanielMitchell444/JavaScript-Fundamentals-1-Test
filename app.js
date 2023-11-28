c6e1385e6cebc5ff0282471e4b6b4bf57c4f5ec2
Daniel Mitchell <49624246+DanielMitchell444@users.noreply.github.com>
2023-11-28 10:57:01 -0800

    Add files via upload

---
+++ app.js
@@ -0,0 +1,20 @@
+//WRITE YOUR CODE UNDER THE COMMENTS //
+
+//declare a variable using constant, it can have any value assigned//
+
+//declare a variable using let, it can have any value assigned //
+
+//declare two variables using let, and add them together
+
+//declare two variables using let, and subtract them
+
+//declare two variables using let, and multiply them
+
+// declare two variables using let, and divide them //
+
+
+//BONUS QUESTION //
+/*  declare two variables using either let or constant    
+    then declare a sum variable and store the sum of the two
+    variables inside sum variable
+*/
